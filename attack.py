2aa3fe4758e829d33574310f2c8893a47ec7275e
iqrarasool04 <iqrarasool45@gmail.com>
2024-05-12 22:40:26 +0500

    commit

--- attack.py
+++ attack.py
@@ -1,28 +1,21 @@
 from algorithm import bin2hex, encrypt
 from key import rkb, rk
-
-# Cipher text to be decrypted
-cipher_text = "3DF950DD4DEB6776"
-print(len(cipher_text))
+from main import output_cipher
+import time
 
 # Generate a subset of keys for demonstration (e.g., first 10 keys)
-all_possible_keys = [format(i, '0120b') for i in range(2**120)]
-all_possible_keys_hex = [bin2hex(key) for key in all_possible_keys]
+all_possible_keys = [format(i, '0120b') for i in range(10000)]
 
+start_time = time.time()
 # Try each key to decrypt the cipher text
 for possible_key in all_possible_keys:
     possible_key_hex = bin2hex(possible_key)
     print("Trying key:", possible_key_hex)
+    decrypted_text = encrypt(output_cipher, rkb, rk)
     
-    # Decrypt the cipher text using the current key
-    decrypted_text = encrypt(cipher_text, rkb, rk)
-    
-    # Check if the decrypted text matches the original plaintext
-    if decrypted_text == cipher_text:
+    # Check if the decrypted text is the original plaintext
+    if decrypted_text == output_cipher:
         print("Key found:", possible_key_hex)
-        break  # Exit the loop if the key is found
-
-    # Print progress every 10,000 keys
-    if index % 10000 == 0:
-        print("Tried {} keys...".format(index))
-
+        break
+end_time = time.time()
+print("Time taken:", end_time - start_time, "seconds")
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from algorithm import bin2hex, hex2bin, encrypt
 from key import rkb, rk, remaining_80_bits
 from round17 import round_17
 from inverseround17 import inverse_round_17
+import time
 
-pt = "123456ABCD132539"
+start_time = time.time()
+pt = "F48456A05D1320B4"
 print("Encryption")
 cipher_text = bin2hex(encrypt(pt, rkb, rk))
 print("Cipher Text : ", cipher_text)
@@ -21,3 +23,5 @@ rkb_rev = rkb[::-1]
 rk_rev = rk[::-1]
 text = bin2hex(encrypt(cipherText, rkb_rev, rk_rev))
 print("Plain Text : ", text)
+end_time = time.time()
+print("Time taken:", end_time - start_time, "seconds")
